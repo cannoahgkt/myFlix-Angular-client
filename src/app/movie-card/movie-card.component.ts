152033e9b58c9ad832aaac7ecf0116eeabbdaff6
Can Noah <87754848+cannoahgkt@users.noreply.github.com>
2024-05-08 22:18:42 +0200

    6.4 changes

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,24 +1,180 @@
 import { Component, OnInit } from '@angular/core';
-import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DirectorInfoComponent } from '../director-info/director-info.component';
+import { GenreInfoComponent } from '../genre-info/genre-info.component';
+import { MovieSynopsisComponent } from '../movie-synopsis/movie-synopsis.component';
 
+// Import to bring in the API call created in 6.2
+import { FetchApiDataService  } from '../fetch-api-data.service';
+
+// Import to display notifications back to the user
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+/**
+ * @description Component representing the movie card.
+ * @selector 'app-movie-card'
+ * @templateUrl './movie-card.component.html'
+ * @styleUrls ['./movie-card.component.scss']
+ */
 @Component({
   selector: 'app-movie-card',
   templateUrl: './movie-card.component.html',
   styleUrls: ['./movie-card.component.scss']
 })
-export class MovieCardComponent {
+export class MovieCardComponent implements OnInit {
+
   movies: any[] = [];
-  constructor(public fetchApiData: FetchApiDataService) { }
+  user: any = {};
+  userData = { Username: "", FavoriteMovies: []};
+  FavoriteMovies: any[] = [];
+  isFavMovie: boolean = false;
 
-ngOnInit(): void {
-  this.getMovies();
-}
+  /**
+   * @constructor - Constructor for MovieCardComponent.
+   * @param {FetchApiDataService} fetchApiData - Service for fetching data from the API.
+   * @param {MatDialog} dialog - Material dialog service for opening dialogs.
+   * @param {MatSnackBar} snackBar - Material snack bar service for displaying notifications.
+   */
+  constructor (
+    public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    ) {}
 
-getMovies(): void {
-  this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+  ngOnInit(): void {
+    this.getMovies();
+    this.getFavMovies();
+  }
+
+  /**
+   * Function for getting all movies.
+   * @returns All movies.
+   */
+  getMovies(): void {
+    this.fetchApiData.getAllMovies().subscribe((resp: any) => {
       this.movies = resp;
       console.log(this.movies);
       return this.movies;
+    });
+  }
+
+  /**
+   * Function that will open the dialog when director button is clicked.
+   * @param {string} name - Name of the director.
+   * @param {string} bio - Biography of the director.
+   * @param {string} birth - Birth date of the director.
+   * @param {string} death - Death date of the director.
+   * @returns Directors name, bio, birth date and death date.
+   */
+  openDirectorDialog(name: string, bio: string, birth: string, death: string): void {
+    this.dialog.open(DirectorInfoComponent, {
+      data: {
+        Name: name,
+        Bio: bio,
+        Birth: birth,
+        Death: death
+      },
+      width: '450px',
+    });
+  }
+
+  /**
+   * Function that will open the dialog when genre button is clicked.
+   * @param {string} name - Name of the genre.
+   * @param {string} description - Description of the genre.
+   * @returns Genre name and discription.
+   */
+  openGenreDialog(name: string, description: string): void {
+    this.dialog.open(GenreInfoComponent, {
+      data: {
+        Name: name,
+        Description: description,
+      },
+      width: '450px',
+    });
+  }
+
+  /**
+   * Function that will open the dialog when synopsis button is clicked
+   * @param {string} description - Description of the movie.
+   * @returns Description of the movie.
+   */
+  openSynopsisDialog(description: string): void {
+    this.dialog.open(MovieSynopsisComponent, {
+      data: {
+        Description: description,
+      },
+      width: '450px',
+    });
+  }
+
+  /**
+   * Function to get favMovie list.
+   * @returns Favorite movies of user.
+   */
+  getFavMovies(): void { 
+    this.user = this.fetchApiData.getUser();
+    this.userData.FavoriteMovies = this.user.FavoriteMovies;
+    this.FavoriteMovies = this.user.FavoriteMovies;
+    console.log('Fav Movies in getFavMovie', this.FavoriteMovies); 
+  }
+
+  /**
+   * Function to check if movie is a favorite movie.
+   * @param movie  - Movie object to check.
+   * @returns {boolean} - Boolean indicating whether the movie is a favorite.
+   */
+  isFav(movie: any): any {
+    const MovieID = movie._id;
+    if (this.FavoriteMovies.some((movie) => movie === MovieID)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Function add / delete favMovie by icon button
+   * @param {any} movie - Movie to toggle favorite icon for. 
+   */
+  toggleFav(movie: any): void {
+    const isFavorite = this.isFav(movie);
+    isFavorite
+      ? this.deleteFavMovies(movie)
+      : this.addFavMovies(movie);
+  }
+
+  /**
+   * Function to add movie to favMovie list
+   * @param {any} movie - Movie to add to favorite movies.
+   * @returns Message "Movie has been added to your favorites!"
+   */
+  addFavMovies(movie: any): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.Username = this.user.Username;
+    this.fetchApiData.addFavouriteMovies(movie).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result));
+      this.getFavMovies(); 
+      this.snackBar.open('Movie has been added to your favorites!', 'OK', {
+        duration: 3000,
+      });
+    });
+  }
+
+  /**
+   * Function to delete movie from favMovie list.
+   * @param {any} movie - Movie to delete from favorite movies.
+   * @returns Message "Movie has been deleted from your favorites!"
+   */
+  deleteFavMovies(movie: any): void {
+    this.user = this.fetchApiData.getUser();
+    this.userData.Username = this.user.Username;
+    this.fetchApiData.deleteFavouriteMovies(movie).subscribe((result) => {
+      localStorage.setItem('user', JSON.stringify(result));
+      this.getFavMovies();
+      this.snackBar.open('Movie has been deleted from your favorites!', 'OK', {
+        duration: 3000,
+      });
     });
   }
 }
